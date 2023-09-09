24f941c88843926aa33220e8354a67cea02d01e1
Sanchit Dilip Jain <sanchitdilipjain@gmail.com>
2023-09-09 22:45:42 +0530

    Create demo.py

---
+++ lambda/demo.py
@@ -0,0 +1,27 @@
+from __future__ import division
+import json
+
+def lambda_handler(event, context):
+   
+   print(event)
+   if 'Records' in event:
+      message = json.loads(event['Records'][0]['body'])
+      number1 = message['Number1']
+      number2 = message['Number2']
+   else:
+      number1 = event['Number1']
+      number2 = event['Number2']
+   
+   sum = number1 + number2
+   product = number1 * number2
+   difference = abs(number1 - number2)
+   quotient = number1 / number2
+   return {
+       "statusCode": 200,
+       "Number1": number1,
+       "Number2": number2,
+       "Sum": sum,
+       "Product": product,
+       "Difference": difference,
+       "Quotient": quotient
+   }
